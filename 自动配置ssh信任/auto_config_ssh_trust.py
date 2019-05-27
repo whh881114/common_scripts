e8899487dba492490b3ce8b8e359b712e1dab439
Roy Wong <whh881114@gmail.com>
2019-05-28 00:58:55 +0800

    已完善脚本功能。

--- 自动配置ssh信任/auto_config_ssh_trust.py
+++ 自动配置ssh信任/auto_config_ssh_trust.py
@@ -55,36 +55,91 @@ def main():
         host_list = []
         for i in f.readlines():
             # 去掉注释内容
-            host_list.append(re.sub(r'(#.*)|(\n)', "", i))
+            j = re.sub(r'(#.*)|(\n)', "", i)
+            if j == '':
+                pass    # 当匹配到以#开头或\n的内容时，直接跳过。
+            else:
+                k = re.sub(r'(\ +)|(\t+)', " ", j)  # 将行内容的多个空格或tab内容统一换成一个空格
+                l = re.sub(r'\ +', " ", k)  # 将行内容的多个空格统一换成一个空格，这个防止\t和\ 相互交叉填写。
+                host_list.append(l)
 
     # 去掉重复内容，并删除空内容''。
     host_list=list(set(host_list))
-    host_list.remove('')
+    # print("Debug " * 20)
     # print(host_list)
+    # print("Debug " * 20)
 
     # 对文件中的每行进行强制性检查格式，只允许有四列，其他情况全记录在日志中。
     for i in host_list:
         if len(i.split(' ')) != 4:
-            logger.warn("%s，此行内容不符标准格式：主机名/IP地址 ssh端口 用户名 密码，只能以一个空格隔开。" % i)
+            logger.warn("%s，此行内容不符标准格式：主机名/IP地址 ssh端口 用户名 密码，支持多个空格或TAB分隔，但是只允许存在四列。" % i)
         else:
-            (host, port, username, password) = a.split(' ')
+            (host, port, username, password) = i.split(' ')
             logger.info("开始为%s配置ssh信任。" % host)
 
             # 设置ps1值，因为建立信息时，不一定是root，所以配置ps1值为root和普通用户提示符。
-            ps1 = '\#|\$'
+            ps1 = '#|$'
             try:
-                cmd = 'ssh-copy-id -p %d %s@$' % (int(port), host, username)
+                port = int(port)
+                cmd = 'ssh-copy-id -f -p %d %s@%s' % (port, username, host)
             except:
-                logger.critical("主机记录异常%s %d %s %s，不能转换成正确的ssh-copy-id命令，跳过此主机记录。" % (host, port, username, password))
-                break
+                logger.critical("主机记录异常%s %s %s %s，不能转换成正确的ssh-copy-id命令，跳过此主机记录。" % (host, port, username, password))
+                continue
 
             child = pexpect.spawn(cmd)
-            index = child.expect(['(yes/no)?', 'assword:', ps1, pexpect.EOF, pexpect.TIMEOUT])
+            index = child.expect([ps1, pexpect.EOF, pexpect.TIMEOUT, 'assword:', '(yes/no)?'])
 
-            # 第一个内容，如果匹配时，还有可能再出现一个'(yes/no)?'，这表示目标主机换过IP地址。
-            # 所以这些还需一个index再做分支判断。
+            # 当主机名故意写错时，index == 0也是成功的，无法用child.before或child.after来匹配内容。所以先使用一个nc命令来测试端口或主机名对不对的情况。
+            cmd = "nc -zv %s %d" % (host, port)
+            retcode = subprocess.call(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            if retcode == 0:
+                pass
+            else:
+                logger.critical("主机记录异常%s %d %s %s，测试主机所对应的端口不通，跳过此主机记录，请检查主机名或端口名。" % (host, port, username, password))
+                continue
 
-            # 承接上面一个，输入错误的密码后，会再次提示'assword:'，所以这时就需要做提示了。
+            if index == 0:
+                # 匹配到ps1，那说明已经配置过了信任。
+                child.sendline('exit')
+                logger.info("主机记录正常%s %d %s %s，此主机已经配置过了信任，忽略此记录。" % (host, port, username, password))
+
+            elif index == 1:
+                # 没有匹配除ps1，'assword:', '(yes/no)?'以外的匹配项。
+                logging.error("主机记录异常%s %d %s %s，建立信任时，没有匹配到所期望的匹配符。" % (host, port, username, password))
+
+            elif index == 2:
+                logging.error("主机记录异常%s %d %s %s，建立信任时，连接超时。" % (host, port, username, password))
+
+            elif index == 3:
+                child.sendline("%s" % password)
+                sub_index = child.expect(['assword:', ps1])
+                if sub_index == 0:
+                    logger.error("主机记录异常%s %d %s %s，建立信任时，用户所对应的密码错误。" % (host, port, username, password))
+                elif sub_index == 1:
+                    child.sendline('exit')
+                    logger.info("主机记录正常%s %d %s %s，此主机已成功配置信任。" % (host, port, username, password))
+
+            elif index == 4:
+                pexpect.spawn('yes')
+
+                # 第一个内容，如果匹配时，还有可能再出现一个'(yes/no)?'，这表示目标主机换过IP地址。所以这些还需一个index再做分支判断。
+                sub_index = child.expect(['(yes/no)?', 'assword:'])
+
+                if sub_index == 0:
+                    child.sendline('yes')
+                    child.expect('assword:')
+                    child.sendline("%s" % password)
+                elif index == 1:
+                    child.sendline("%s" % password)
+
+                # 此时再检查密码是否输入错误。
+                sub_index = child.expect(['assword:', ps1])
+                if sub_index == 0:
+                    logger.error("主机记录异常%s %d %s %s，建立信任时，用户所对应的密码错误。" % (host, port, username, password))
+                elif sub_index == 1:
+                    child.sendline('exit')
+                    logger.info("主机记录正常%s %d %s %s，此主机已成功配置信任。" % (host, port, username, password))
+
 
 
 if __name__ == "__main__":
